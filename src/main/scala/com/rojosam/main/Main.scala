1f4d92cf0ef9b4e4863831605e6dcbbfac2a0a5f
lalosam <lalosam369@gmail.com>
2016-05-07 12:11:55 -0500

    Improve examples selection

--- src/main/scala/com/rojosam/main/Main.scala
+++ src/main/scala/com/rojosam/main/Main.scala
@@ -8,7 +8,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     println(s"Arguments: ${args.mkString(", ")}")
-    val example = args(0).toInt
+    val example = args(0)
     val input = args(1)
     val output = if(args.length > 2) {
       Some(args(2))
@@ -20,13 +20,13 @@ object Main {
     val letters = sc.parallelize('a' to 'z')
     val dictionary = sc.textFile(input)
     example match {
-      case 1 =>
+      case "filter" =>
         Filter.execute(dictionary)
-      case 2 =>
+      case " " =>
         //Example2.execute(sc, args)
-      case 3 =>
+      case "StatisticsByLetter" =>
         StatisticsByLetter.execute(dictionary)
-      case 4 =>
+      case "Acronyms" =>
         Acronyms.execute(dictionary)
     }
     if(!sc.isStopped) sc.stop()
