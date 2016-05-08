4015dbcfc90534d4fb680d2d19fdb97a6c209ec8
lalosam <lalosam369@gmail.com>
2016-05-08 12:33:45 -0500

    Tune partition size

--- src/main/scala/com/rojosam/examples/GenerateStrings.scala
+++ src/main/scala/com/rojosam/examples/GenerateStrings.scala
@@ -13,13 +13,23 @@ object GenerateStrings {
 
   def generateStrings (letters:RDD[String]): RDD[String] = {
     val chars = letters.cache()
+    printSize("CHARS", chars)
     val chars2 = chars.cartesian(chars).map(concat)
+    printSize("CHARS2", chars2)
     val chars3 = chars2.cartesian(chars).map(concat)
+    printSize("CHARS3", chars3)
     val chars4 = chars3.cartesian(chars).map(concat)
-    chars.union(chars2).union(chars3).union(chars4)
+    printSize("CHARS4", chars4)
+    val result = chars.union(chars2).union(chars3).union(chars4)
+    printSize("RESULT", result)
+    result
   }
 
   def concat(t: (String, String)):String = {
     t._1 + t._2
+  }
+
+  def printSize(name:String, rdd:RDD[String]): Unit ={
+    println(s"******* **** ****   $name: ${rdd.partitions.size}")
   }
 }
